9af7d0e0e58c7211bc9016e139e753b5300c365d
Mun Kyoung-min <mun9659@naver.com>
2022-11-13 00:46:21 +0900

    [rename] Customer renamed Money

---
+++ src/main/java/lotto/domain/Money.java
@@ -0,0 +1,41 @@
+package lotto.domain;
+
+public class Money {
+	
+	private final static int MINIMUM_MONEY = 1000;
+	private final int money;
+	
+	public Money(String input)  {
+		int money = moneyCheck(input);
+		dividedCheck(money);
+		lowMoneyCheck(money);
+		this.money = money;
+	}
+	
+	public int getMoney() {
+		return money;
+	}
+	
+	public static int moneyCheck(String money) {
+		try {
+			return Integer.parseInt(money);
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException("[ERROR] 구입 금액은 숫자로 입력 받아야 합니다.");
+		}
+	}
+	
+	public static void dividedCheck(int money) {
+		if(money % MINIMUM_MONEY == 0) {
+			return;
+		}
+		// System.out.println("[ERROR] 구입 금액은 1000원 단위로 입력 받아야 합니다.");
+		throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위로 입력 받아야 합니다.");
+	}
+	
+	public static void lowMoneyCheck(int money) {
+		if(money < MINIMUM_MONEY) {
+			// System.out.println("[ERROR] 구입 금액은 1000원 이상이여야 합니다.");
+			throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 이상이여야 합니다.");
+		}
+	}
+}
