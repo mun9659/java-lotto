5da1d938784ebb513d898292854ab461783d83be
Mun Kyoung-min <mun9659@naver.com>
2022-11-13 01:21:33 +0900

    [feat] make lotto, view lotto , sorting lotto add

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -4,15 +4,30 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Lotto {
+	private static final int MIN_RANGE_NUMBER = 1;
+	private static final int MAX_RANGE_NUMBER = 45;
+	private static final int MAXIMUM_LENGTH = 6;
+	
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         duplicate(numbers);
+        numbers.sort(null);
         this.numbers = numbers;
     }
-
+    
+    public static Lotto newLotto() {
+    	return new Lotto(Randoms.pickUniqueNumbersInRange(MIN_RANGE_NUMBER, MAX_RANGE_NUMBER, MAXIMUM_LENGTH));
+    }
+    
+    public List<Integer> getLottoNumbers() {
+    	return numbers;
+    }
+    
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
