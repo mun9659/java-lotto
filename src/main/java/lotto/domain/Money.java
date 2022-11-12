0ad413f76d2e5579079a8774d47abff192a3ea90
Mun Kyoung-min <mun9659@naver.com>
2022-11-13 01:19:55 +0900

    [feat] Lotto count add

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -16,10 +16,15 @@ public class Money {
 		return money;
 	}
 	
+	public int getLottoCount() {
+		return money / MINIMUM_MONEY;
+	}
+	
 	public static int moneyCheck(String money) {
 		try {
 			return Integer.parseInt(money);
 		} catch (NumberFormatException e) {
+			System.out.println("[ERROR] 구입 금액은 숫자로 입력 받아야 합니다.");
 			throw new IllegalArgumentException("[ERROR] 구입 금액은 숫자로 입력 받아야 합니다.");
 		}
 	}
@@ -28,13 +33,13 @@ public class Money {
 		if(money % MINIMUM_MONEY == 0) {
 			return;
 		}
-		// System.out.println("[ERROR] 구입 금액은 1000원 단위로 입력 받아야 합니다.");
+		System.out.println("[ERROR] 구입 금액은 1000원 단위로 입력 받아야 합니다.");
 		throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위로 입력 받아야 합니다.");
 	}
 	
 	public static void lowMoneyCheck(int money) {
 		if(money < MINIMUM_MONEY) {
-			// System.out.println("[ERROR] 구입 금액은 1000원 이상이여야 합니다.");
+			System.out.println("[ERROR] 구입 금액은 1000원 이상이여야 합니다.");
 			throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 이상이여야 합니다.");
 		}
 	}
